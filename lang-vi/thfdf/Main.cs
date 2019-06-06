98ae27a373e3c2ad3ca6c2fd54dee68bbfa4a2ed
Meigyoku Thmn <bestfriends_0705@yahoo.com.vn>
2019-06-07 00:16:07 +0700

    Add steam loading

--- lang-vi/thfdf/Main.cs
+++ lang-vi/thfdf/Main.cs
@@ -1,10 +1,15 @@
 //css_import Detours.cs
 //css_import Transpilers.cs
 //css_import Types.cs
+using System;
+using System.Runtime.InteropServices;
 namespace DotnetPatching {
    class Config {
+      [DllImport("steam_api.dll", SetLastError = true)]
+      public static extern bool SteamAPI_Init();
       static void OnInit() {
-
+         try { SteamAPI_Init(); }
+         catch (Exception e) { Console.WriteLine(e.ToString()); }
       }
    }
 }
