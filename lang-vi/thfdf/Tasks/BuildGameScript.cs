d4f73310a991452b60072535a4f56babcdbe97e0
Meigyoku Thmn <bestfriends_0705@yahoo.com.vn>
2019-08-13 22:58:40 +0700

    let the csscript assembly resolver do everything

--- lang-vi/thfdf/Tasks/BuildGameScript.cs
+++ lang-vi/thfdf/Tasks/BuildGameScript.cs
@@ -3,7 +3,6 @@
 //css_dir %css_nuget%\CsvHelper\**\net45
 //css_dir %css_nuget%\CsvHelper\System.ValueTuple*\**\net461
 //css_ref CsvHelper.dll
-//css_ref System.ValueTuple.dll
 //css_import BuildGameScript\DialogConv.cs
 //css_import BuildGameScript\MusicConv.cs
 //css_import BuildGameScript\AchiveConv.cs
--- lang-vi/thfdf/Tasks/Extractor.cs
+++ lang-vi/thfdf/Tasks/Extractor.cs
@@ -1,5 +1,5 @@
 //css_args -provider:%CSSCRIPT_DIR%\lib\CSSRoslynProvider.dll
-//css_ref %CSSCRIPT_DIR%\lib\Bin\Roslyn\System.ValueTuple.dll
+//css_dir %CSSCRIPT_DIR%\lib\Bin\Roslyn
 //css_import Extractor\Decipher.cs
 using System;
 using System.Collections.Generic;
@@ -11,46 +11,50 @@ using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
-class Program : IWin32Window {
-  public IntPtr Handle { 
-    get { return GetConsoleWindow(); } 
-  }
-  [DllImport("kernel32.dll")]
-  static extern IntPtr GetConsoleWindow();
-  [STAThread]
-  static void Main(string[] args) {
-     var consoleWindow = new Program();
-     var errorStrings = new List<string>();
-     string lastRootPath = null;
-     string lastOutputPath = null;
-     try { lastRootPath = File.ReadAllText("rootPath.txt"); } catch (Exception) { }
-     try { lastOutputPath = File.ReadAllText("outputPath.txt"); } catch (Exception) { }
-     var folderDialog = new OpenFileDialog();
-     if (lastRootPath != null) folderDialog.InitialDirectory = lastRootPath;
-     folderDialog.Title = "Select game's directory";
-     folderDialog.FileName = "Access to a directory then type any name at here";
-     folderDialog.CheckFileExists = false;
-     var dialogRs = folderDialog.ShowDialog(consoleWindow);
-     if (dialogRs == DialogResult.Cancel) return;
-     var rootPath = Path.GetDirectoryName(folderDialog.FileName) + '\\';
-     File.WriteAllText("rootPath.txt", rootPath);
-     if (lastOutputPath != null) folderDialog.InitialDirectory = lastOutputPath;
-     folderDialog.Title = "Select base output directory";
-     folderDialog.FileName = "Access to a directory then type any name at here";
-     folderDialog.CheckFileExists = false;
-     dialogRs = folderDialog.ShowDialog(consoleWindow);
-     if (dialogRs == DialogResult.Cancel) return;
-     var outputPath = Path.GetDirectoryName(folderDialog.FileName);
-     File.WriteAllText("outputPath.txt", outputPath);
-     Console.WriteLine("Enter output folder name:");
-     string outputName = "";
-     while (outputName.Length == 0) {
-        outputName = Console.ReadLine();
-        outputName = outputName.Trim();
-     }
-     var graphicsPath = @"Content\Graphics";
-     var dataPath = @"Content\Data";
-     var dataFilePatterns = new[] {
+class Program : IWin32Window
+{
+   public IntPtr Handle
+   {
+      get { return GetConsoleWindow(); }
+   }
+   [DllImport("kernel32.dll")]
+   static extern IntPtr GetConsoleWindow();
+   [STAThread]
+   static void Main(string[] args)
+   {
+      var consoleWindow = new Program();
+      var errorStrings = new List<string>();
+      string lastRootPath = null;
+      string lastOutputPath = null;
+      try { lastRootPath = File.ReadAllText("rootPath.txt"); } catch (Exception) { }
+      try { lastOutputPath = File.ReadAllText("outputPath.txt"); } catch (Exception) { }
+      var folderDialog = new OpenFileDialog();
+      if (lastRootPath != null) folderDialog.InitialDirectory = lastRootPath;
+      folderDialog.Title = "Select game's directory";
+      folderDialog.FileName = "Access to a directory then type any name at here";
+      folderDialog.CheckFileExists = false;
+      var dialogRs = folderDialog.ShowDialog(consoleWindow);
+      if (dialogRs == DialogResult.Cancel) return;
+      var rootPath = Path.GetDirectoryName(folderDialog.FileName) + '\\';
+      File.WriteAllText("rootPath.txt", rootPath);
+      if (lastOutputPath != null) folderDialog.InitialDirectory = lastOutputPath;
+      folderDialog.Title = "Select base output directory";
+      folderDialog.FileName = "Access to a directory then type any name at here";
+      folderDialog.CheckFileExists = false;
+      dialogRs = folderDialog.ShowDialog(consoleWindow);
+      if (dialogRs == DialogResult.Cancel) return;
+      var outputPath = Path.GetDirectoryName(folderDialog.FileName);
+      File.WriteAllText("outputPath.txt", outputPath);
+      Console.WriteLine("Enter output folder name:");
+      string outputName = "";
+      while (outputName.Length == 0)
+      {
+         outputName = Console.ReadLine();
+         outputName = outputName.Trim();
+      }
+      var graphicsPath = @"Content\Graphics";
+      var dataPath = @"Content\Data";
+      var dataFilePatterns = new[] {
         ( @"^0\.xna$", 2, "Text" ),
         ( @"^1\.xna$", 2, "Text" ),
         ( @"^2\.xna$", 2, "Text" ),
@@ -70,75 +74,85 @@ class Program : IWin32Window {
         ( @"^s.+?\.xna$", 2, "Text" ),
         ( @"^e.+?\.xna$", 0, "Text" ),
      };
-     var graphicsFileNames = Directory.GetFiles(Path.Combine(rootPath, graphicsPath), "*", SearchOption.AllDirectories);
-     var formats = new SortedSet<string>();
-     foreach (var fileName in graphicsFileNames) {
-        try {
-           Image.FromStream(Decipher.Decry(fileName, 0), true, true);
-           var texture = Image.FromStream(Decipher.Decry(fileName, 0), true, true);
-           var newFileName = fileName.Replace(rootPath, "");
-           Console.WriteLine(newFileName);
-           newFileName = Path.Combine(outputPath, outputName, newFileName);
-           var newDirName = Path.GetDirectoryName(newFileName);
-           Directory.CreateDirectory(newDirName);
-           newFileName = newFileName + ".png";
-           texture.Save(newFileName, ImageFormat.Png);
-        }
-        catch (Exception e) {
-           errorStrings.Add("When processing file: " + fileName.Replace(rootPath, ""));
-           errorStrings.Add(e.ToString());
-        }
-     }
-     var dataFileNames = Directory.GetFiles(Path.Combine(rootPath, dataPath), "*", SearchOption.AllDirectories);
-     foreach (var filePath in dataFileNames) {
-        try {
-           var fileName = Path.GetFileName(filePath);
-           var tt = dataFilePatterns.FirstOrDefault(e => new Regex(e.Item1).IsMatch(fileName));
-           if (tt.Item1 == null) {
-              var stream = Decipher.Decry(filePath, 2);
-              byte[] buffer = new byte[stream.Length];
-              stream.Read(buffer, 0, (int)stream.Length);
-              var newFilePath = filePath.Replace(rootPath, "");
-              Console.WriteLine(newFilePath);
-              newFilePath = Path.Combine(outputPath, outputName, newFilePath);
-              var newDirName = Path.GetDirectoryName(newFilePath);
-              Directory.CreateDirectory(newDirName);
-              newFilePath = newFilePath + ".txt";
-              File.WriteAllBytes(newFilePath, buffer);
-           }
-           else {
-              var stream = Decipher.Decry(filePath, tt.Item2);
-              if (tt.Item3 == "Text") {
-                 byte[] buffer = new byte[stream.Length];
-                 stream.Read(buffer, 0, (int)stream.Length);
-                 var newFilePath = filePath.Replace(rootPath, "");
-                 Console.WriteLine(newFilePath);
-                 newFilePath = Path.Combine(outputPath, outputName, newFilePath);
-                 var newDirName = Path.GetDirectoryName(newFilePath);
-                 Directory.CreateDirectory(newDirName);
-                 newFilePath = newFilePath + ".txt";
-                 File.WriteAllBytes(newFilePath, buffer);
-              }
-              else if (tt.Item3 == "Texture2D") {
-                 var texture = Image.FromStream(stream);
-                 var newFilePath = filePath.Replace(rootPath, "");
-                 Console.WriteLine(newFilePath);
-                 newFilePath = Path.Combine(outputPath, outputName, newFilePath);
-                 var newDirName = Path.GetDirectoryName(newFilePath);
-                 Directory.CreateDirectory(newDirName);
-                 newFilePath = newFilePath + ".png";
-                 texture.Save(newFilePath, ImageFormat.Png);
-              }
-           }
-        }
-        catch (Exception e) {
-           errorStrings.Add("When processing file: " + filePath.Replace(rootPath, ""));
-           errorStrings.Add(e.ToString());
-        }
-     }
-     Console.WriteLine("Number of errors: " + errorStrings.Count);
-     File.WriteAllLines("Error.txt", errorStrings.ToArray());
-     Console.WriteLine("Press enter to exit.");
-     Console.ReadLine();
-  }
+      var graphicsFileNames = Directory.GetFiles(Path.Combine(rootPath, graphicsPath), "*", SearchOption.AllDirectories);
+      var formats = new SortedSet<string>();
+      foreach (var fileName in graphicsFileNames)
+      {
+         try
+         {
+            Image.FromStream(Decipher.Decry(fileName, 0), true, true);
+            var texture = Image.FromStream(Decipher.Decry(fileName, 0), true, true);
+            var newFileName = fileName.Replace(rootPath, "");
+            Console.WriteLine(newFileName);
+            newFileName = Path.Combine(outputPath, outputName, newFileName);
+            var newDirName = Path.GetDirectoryName(newFileName);
+            Directory.CreateDirectory(newDirName);
+            newFileName = newFileName + ".png";
+            texture.Save(newFileName, ImageFormat.Png);
+         }
+         catch (Exception e)
+         {
+            errorStrings.Add("When processing file: " + fileName.Replace(rootPath, ""));
+            errorStrings.Add(e.ToString());
+         }
+      }
+      var dataFileNames = Directory.GetFiles(Path.Combine(rootPath, dataPath), "*", SearchOption.AllDirectories);
+      foreach (var filePath in dataFileNames)
+      {
+         try
+         {
+            var fileName = Path.GetFileName(filePath);
+            var tt = dataFilePatterns.FirstOrDefault(e => new Regex(e.Item1).IsMatch(fileName));
+            if (tt.Item1 == null)
+            {
+               var stream = Decipher.Decry(filePath, 2);
+               byte[] buffer = new byte[stream.Length];
+               stream.Read(buffer, 0, (int)stream.Length);
+               var newFilePath = filePath.Replace(rootPath, "");
+               Console.WriteLine(newFilePath);
+               newFilePath = Path.Combine(outputPath, outputName, newFilePath);
+               var newDirName = Path.GetDirectoryName(newFilePath);
+               Directory.CreateDirectory(newDirName);
+               newFilePath = newFilePath + ".txt";
+               File.WriteAllBytes(newFilePath, buffer);
+            }
+            else
+            {
+               var stream = Decipher.Decry(filePath, tt.Item2);
+               if (tt.Item3 == "Text")
+               {
+                  byte[] buffer = new byte[stream.Length];
+                  stream.Read(buffer, 0, (int)stream.Length);
+                  var newFilePath = filePath.Replace(rootPath, "");
+                  Console.WriteLine(newFilePath);
+                  newFilePath = Path.Combine(outputPath, outputName, newFilePath);
+                  var newDirName = Path.GetDirectoryName(newFilePath);
+                  Directory.CreateDirectory(newDirName);
+                  newFilePath = newFilePath + ".txt";
+                  File.WriteAllBytes(newFilePath, buffer);
+               }
+               else if (tt.Item3 == "Texture2D")
+               {
+                  var texture = Image.FromStream(stream);
+                  var newFilePath = filePath.Replace(rootPath, "");
+                  Console.WriteLine(newFilePath);
+                  newFilePath = Path.Combine(outputPath, outputName, newFilePath);
+                  var newDirName = Path.GetDirectoryName(newFilePath);
+                  Directory.CreateDirectory(newDirName);
+                  newFilePath = newFilePath + ".png";
+                  texture.Save(newFilePath, ImageFormat.Png);
+               }
+            }
+         }
+         catch (Exception e)
+         {
+            errorStrings.Add("When processing file: " + filePath.Replace(rootPath, ""));
+            errorStrings.Add(e.ToString());
+         }
+      }
+      Console.WriteLine("Number of errors: " + errorStrings.Count);
+      File.WriteAllLines("Error.txt", errorStrings.ToArray());
+      Console.WriteLine("Press enter to exit.");
+      Console.ReadLine();
+   }
 }
